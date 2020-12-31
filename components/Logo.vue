4337cda4b9a532b0faceba6249c6d3b91d37f601
Bastiaan van de Kamer <bas@duchambre.media>
2020-12-31 10:23:58 +0100

    :tada:  First commitment

---
+++ components/Logo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <img
+      width="240"
+      class="mx-auto"
+      src="@/assets/pet.png"
+      alt="Hoe staat de pet"
+    />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title text-4xl">Hoe staat de pet nu dan?</h1>
+      <div class="links">
+        <table class="table-auto mx-auto">
+          <thead>
+            <tr>
+              <th class="w-1/2 text-left">Persoontje</th>
+              <th class="w-1/2 text-left">Status pet</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="text-left">Bas</td>
+              <td class="text-left">Wij komen</td>
+            </tr>
+            <tr>
+              <td class="text-left">Borek</td>
+              <td class="text-left">Daardoor dus blij!</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+td,
+th {
+  @apply p-1;
+}
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
